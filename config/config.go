5ee0e18f78f6f11cb6352c69b0226c06e6127717
obasajujoshua31 <obasajujoshua31@gmail.com>
2019-09-09 14:33:18 +0100

    added files

--- config/config.go
+++ config/config.go
@@ -2,39 +2,45 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 var (
 	PORT       = 6800
 	DBURL      = ""
-	DBHOST     = "db"
-	DBNAME     = "Movies"
-	DBPASSWORD = "example"
-	DBDRIVER   = "postgres"
-	DBUSERNAME = "joshua"
-	SECRETKEY  = "jesus"
+	DBHOST     = ""
+	DBNAME     = ""
+	DBPASSWORD = ""
+	DBDRIVER   = ""
+	DBUSERNAME = ""
+	SECRETKEY  = ""
 )
 
 func Load() {
-	// var err error
+	var err error
 
-	// err := godotenv.Load(filepath.Join("../", ".env"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	err = godotenv.Load(filepath.Join("../", ".env"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
-	// if err != nil {
-	// 	log.Println(err)
-	// 	PORT = 6800
-	// }
+	PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		log.Println(err)
+		PORT = 6800
+	}
 
-	// SECRETKEY = os.Getenv("SECRET_KEY")
+	SECRETKEY = os.Getenv("SECRET_KEY")
 
-	// DBHOST = os.Getenv("DB_HOST")
-	// DBUSERNAME = os.Getenv("DB_USER")
-	// DBPASSWORD = os.Getenv("DB_PASSWORD")
-	// DBDRIVER = os.Getenv("DB_DRIVER")
-	// DBNAME = os.Getenv("DB_NAME")
+	DBHOST = os.Getenv("DB_HOST")
+	DBUSERNAME = os.Getenv("DB_USER")
+	DBPASSWORD = os.Getenv("DB_PASSWORD")
+	DBDRIVER = os.Getenv("DB_DRIVER")
+	DBNAME = os.Getenv("DB_NAME")
 	DBURL = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", DBHOST, DBUSERNAME, DBNAME, DBPASSWORD)
 }
